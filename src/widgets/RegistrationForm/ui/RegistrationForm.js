1484a24dc711fa34bb2cca4a6d4eaf3be4ac21c2
jmark1518 <markarova2301@gmail.com>
2026-03-09 23:40:37 +0300

    Update RegistrationForm.js

--- src/widgets/RegistrationForm/ui/RegistrationForm.js
+++ src/widgets/RegistrationForm/ui/RegistrationForm.js
@@ -9,11 +9,11 @@ export const RegistrationForm = async (props) => {
         nicknameField: Field({
             className: 'registration-form__field',
             id: 'nickname-input',
-            label: 'Введите ник',
+            label: 'Введите никнейм',
             type: 'login',
             attributes: {
                 name: 'nickname',
-                placeholder: 'Ник',
+                placeholder: 'Никнейм',
             },
             hasIcon: false,
         }),
