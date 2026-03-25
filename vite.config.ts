04074b18c20007dc963f4fd7be2eacb8bcf3ca49
BurmatovStepan <b9036735842@gmail.com>
2026-03-25 21:17:04 +0300

    build: add target build option in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { handlebarsPrecompile } from 'vite-plugin-handlebars-precompile';
 import * as path from 'path';
 
 export default defineConfig({
+    build: {
+        target: 'es2022',
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src/'),
