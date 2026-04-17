14e0da1d618693fd8c4ffbeaf81c4a0229d918fd
BurmatovStepan <b9036735842@gmail.com>
2026-04-17 13:18:00 +0300

    feat(shared): add Toast

---
+++ src/shared/ui/Toast/ui/Toast.ts
@@ -0,0 +1,17 @@
+import './style.scss';
+
+import { stringToElement } from '@/shared/utils';
+
+import { ToastProps } from '../model/types';
+import template from './Toast.hbs?compiled';
+
+export const Toast = (props: ToastProps) => {
+    const toast = stringToElement(template(props));
+    document.body.appendChild(toast);
+    setTimeout(() => fade(toast), 3000);
+}
+
+const fade = (toast: HTMLElement) => {
+    toast.style.opacity = '0';
+    setTimeout(() => toast.remove(), 300)
+}
